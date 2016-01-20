96cea3e2a4c84c4cac4a57c14b584ae12aa7d580
Lithane <plok314159265@163.com>
2016-01-20 22:08:45 +0800

    Build 49 No update

--- B9 PWings Fork/UIUtility.cs
+++ B9 PWings Fork/UIUtility.cs
@@ -103,8 +103,6 @@ namespace WingProcedural
 
 
             value = Mathf.Clamp((float)(value01 * range + range * delta) , limits.x, limits.y); // lower limit is halved so the fine control can reduce it further but the normal tweak still snaps. Min makes -ve values work
-           // if (isOffset == 1)
-           //     value -= (float)range / 2;
             //value = (float)(value01 * range + limits.x);  //releases clamp
             changed = valueOld != value ? true : false;
 
