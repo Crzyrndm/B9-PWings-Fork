b52678414d3ee3423c9b3da938a2936e41fbc8b9
Lithane <plok314159265@163.com>
2016-03-29 19:10:29 +0800

    merge2

---
+++ B9 PWings Fork/StaticWingGlobals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WingProcedural
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class StaticWingGlobals : MonoBehaviour
+    {
+        public static List<WingTankConfiguration> wingTankConfigurations = new List<WingTankConfiguration>();
+
+        public void Start()
+        {
+            ConfigNode[] nodes = GameDatabase.Instance.GetConfigNodes("ProceduralWingFuelSetups").FirstOrDefault().GetNodes("FuelSet");
+            for (int i = 0; i < nodes.Length; ++i )
+                wingTankConfigurations.Add(new WingTankConfiguration(nodes[i]));
+        }
+    }
+}
